e2df0ef7edb4e6643b872c4f329e12568c86129a
unknown <yang.david@wustl.edu>
2020-08-31 14:43:29 -0500

    Updates

---
+++ precache-manifest.d761903851b650161f913a814d38a766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf457484e1327e85fe7201415862a2c9",
+    "url": "/mealplanner/index.html"
+  },
+  {
+    "revision": "f2e89e7ea62a7298ddc0",
+    "url": "/mealplanner/static/css/main.cac2b5f3.chunk.css"
+  },
+  {
+    "revision": "6ed888cce3c95eb63698",
+    "url": "/mealplanner/static/js/2.ea44898e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mealplanner/static/js/2.ea44898e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e89e7ea62a7298ddc0",
+    "url": "/mealplanner/static/js/main.afae07b3.chunk.js"
+  },
+  {
+    "revision": "058960ce98fbe60808a9",
+    "url": "/mealplanner/static/js/runtime-main.0125c922.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/mealplanner/static/media/logo.8dad8028.svg"
+  }
+]);
